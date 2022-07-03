0f8bd5885b7e55189c5e023a488a219f262ce301
Yoichiro Hasebe <yohasebe@gmail.com>
2022-07-03 14:57:27 +0900

    Precompile JSON files to improve performance

---
+++ source/compile.rb
@@ -0,0 +1,50 @@
+$:.unshift File.dirname(__FILE__)
+require 'template.rb'
+require 'json'
+
+class Hash
+  def deep_merge(*others)
+    self.clone.deep_merge!(*others)
+  end
+
+  def deep_merge!(*others)
+    _child_merge = -> (_, self_val, other_val){
+      if self_val.instance_of?(Hash) && other_val.instance_of?(Hash)
+        self_val.merge other_val, &_child_merge
+      else
+        other_val
+      end
+    }
+    self.merge! *others, &_child_merge
+  end
+end
+
+def preprocess(json)
+  results = json.gsub(/<[^<>]+>([^<>]+)<\/[^<>]+>/){$1}
+  results.gsub!(/'\(\)'/){""}
+  results.gsub!(/\([^\(\)]+\)/){""}
+  results
+end
+
+fcurrent = File.dirname(__FILE__)
+dcurrent = File.expand_path(fcurrent)
+
+base_json= File.read(File.join(dcurrent, "base.json"))
+base_data  = JSON.parse(base_json)
+en_json= File.read(File.join(dcurrent, "locales", "en_us.json"))
+en_data  = JSON.parse(preprocess(en_json))
+en_data = base_data.deep_merge en_data
+
+Dir.foreach(File.join(dcurrent, "locales")) do |item|
+  next if File.extname(item) != ".json"
+  locale = File.basename(item, ".json")
+  locale_json= File.read(File.join(dcurrent, "locales", item))
+  locale_data  = JSON.parse(preprocess(locale_json))
+  data = base_data.deep_merge locale_data
+  compiled = apply_template(en_data, data).to_json
+  fcompiled = File.join(dcurrent, "compiled", locale + ".json")
+  File.open(fcompiled, "w") do |f|
+    f.write compiled
+  end
+end
+
